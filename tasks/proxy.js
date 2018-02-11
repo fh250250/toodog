7646c25b1438dc02c6f9b62468fb1df442f41e8b
Gasper <duanpengyuan@qq.com>
2018-02-11 21:57:34 +0800

    add proxy task

---
+++ tasks/proxy.js
@@ -0,0 +1,25 @@
+'use strict'
+const { Task, api } = require('actionhero')
+
+module.exports = class ProxyTask extends Task {
+  constructor () {
+    super()
+    this.name = 'proxy'
+    this.description = 'run proxy check or crawl'
+    this.frequency = 10000
+  }
+
+  async run (data) {
+    const col = api.mongo.db.collection('proxy')
+    const total = await col.count()
+    const count = await col.find({ rank: { $gte: 20 } }).count()
+
+    if (total < 500 || count / total > 0.2) {
+      api.log('[Proxy] Crawl...')
+      await api.proxy.crawl()
+    }
+
+    api.log('[Proxy] Check...')
+    await api.proxy.check()
+  }
+}
